5c9968744ab16945558f7442da61e20bb14d2f36
SADHANA <chaudharysadhana855@gmail.com>
2024-03-22 20:55:03 +0530

    22-03-2024

---
+++ android/app/src/main/kotlin/com/example/flutter_program/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_program
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+// todo lecture - 1
+import 'package:flutter/material.dart';
+void main() {
+  return runApp(
+    const  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'flutter demo app',
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text(
+            'Hello\n\n\n\nDart\n\n\n\nFlutter',
+            style: TextStyle(
+                color: Colors.red,
+                fontSize: 40,
+                fontWeight: FontWeight.w500,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.yellowAccent,
+                decorationStyle: TextDecorationStyle.double),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+
+// todo lecture - 2
+// import 'package:flutter/material.dart';
+// void main() {
+//   return runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'flutter demo app',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           centerTitle: true,
+//           backgroundColor: Colors.redAccent,
+//           leading: const Icon(
+//             Icons.menu,
+//             color: Colors.white,
+//             size: 25,
+//           ),
+//           title: const Text(
+//             'Flutter App',
+//             style: TextStyle(
+//                 color: Colors.white, fontWeight: FontWeight.w500, fontSize: 25),
+//           ),
+//         ),
+//         body: const Center(
+//           child: Text(
+//             '\t \t \t Red & White Group of Institutes\nOne Step in Changing Education Chain...',
+//             style: TextStyle(
+//                 color: Colors.redAccent,
+//                 fontSize: 22,
+//                 fontWeight: FontWeight.w500),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+
+// todo lecture - 3
+// import 'package:flutter/material.dart';
+// void main() {
+//   return runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'flutter demo app',
+//       home: Scaffold(
+//         backgroundColor: Colors.black,
+//         appBar: AppBar(
+//           centerTitle: true,
+//           backgroundColor: Colors.amber,
+//           leading:const Icon(Icons.menu),
+//           title:const Text('Flutter App',style: TextStyle(fontWeight: FontWeight.bold),),
+//         ),
+//         body:const Center(
+//           child: Text(
+//             'Red & White',
+//             style: TextStyle(
+//                 color: Colors.redAccent,
+//                 fontSize: 55,
+//                 fontWeight: FontWeight.w500,
+//                 decoration: TextDecoration.underline,
+//                 decorationColor: Colors.amber,
+//                 decorationStyle: TextDecorationStyle.double),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
